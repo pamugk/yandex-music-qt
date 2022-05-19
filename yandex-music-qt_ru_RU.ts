1dd5f11223eefad6d070b9199c381dbbdeab11c0
Alexander Permyakov <pamugk@gmail.com>
2022-05-19 09:10:01 +0500

    App initial state
    - set up basic app layout,
    - added official Yandex Music icons,
    - added FontAwesome fonts & svg icons

---
+++ yandex-music-qt_ru_RU.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Яндекс Музыка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <source>Трек, альбом, исполнитель, ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <source>Главная</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="246"/>
+        <source>HQ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="145"/>
+        <source>Подкасты и книги</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="159"/>
+        <source>Радио</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
